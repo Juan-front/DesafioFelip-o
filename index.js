79a414227066f7fe3f6da1db9b87789fcaf232ce
Juan <juan_sl90@outlook.com>
2025-08-24 20:46:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// Definindo os dados do herói
+const nome = "Arthas";
+const xp = 12340; // Experiência do herói
+
+let nivel; // Variável que vai guardar o nível do herói
+
+// Estrutura de decisão para definir o nível
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+}
+
+// Exibindo o resultado
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
